c3258c244349ad7ace91172cb96f945f2a163e4d
danushka96 <danushkaherath96@gmail.com>
2025-02-16 13:13:34 +0530

    added util function

---
+++ string_utils.go
@@ -0,0 +1,26 @@
+package ginboot
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GetRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func GetRandomAlphanumericString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
